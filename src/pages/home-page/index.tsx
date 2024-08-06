31ec391cb1bee56c68a9d142268bb133eb11ec87
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-08-06 11:47:00 -0300

    refactor: move header element to a component

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,76 +1,14 @@
-import { useEffect, useState } from 'react';
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { HiOutlineSpeakerWave } from 'react-icons/hi2';
-import { IoMenu } from 'react-icons/io5';
 import { PiMusicNotesSimple } from 'react-icons/pi';
 import { SlMicrophone } from 'react-icons/sl';
 
+import { Header } from '../../components/Header';
+
 export function HomePage() {
-  const [bgBlur, setBgBlur] = useState('');
-  const [menuIsVisible, setMenuIsVisible] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setBgBlur('backdrop-blur-sm');
-    }, 100);
-  }, []);
-
   return (
     <>
-      <header className='flex justify-center items-start header-image w-full h-[400px]'>
-        <div
-          className={`flex flex-col justify-center items-start h-full w-full ${bgBlur} transition duration-500`}
-        >
-          {menuIsVisible && (
-            <nav className='hidden md:flex justify-center items-center py-6 w-full select-none'>
-              <ul className='flex gap-10 text-lg font-bold'>
-                <li className='text-zinc-200 hover:text-zinc-50'>
-                  <a href=''>Home</a>
-                </li>
-                <li className='text-zinc-200 hover:text-zinc-50'>
-                  <a href='#sobre'>Sobre</a>
-                </li>
-                <li className='text-zinc-200 hover:text-zinc-50'>
-                  <a href='#servicos'>Serviços</a>
-                </li>
-                <li className='text-zinc-200 hover:text-zinc-50'>
-                  <a href='#contato'>Contato</a>
-                </li>
-              </ul>
-            </nav>
-          )}
-
-          <IoMenu
-            onClick={() => setMenuIsVisible(true)}
-            className='right-5 top-5 text-zinc-200 hover:text-zinc-100 fixed cursor-pointer visible md:invisible transition-colors'
-            size={45}
-            height={150}
-          />
-
-          <div className='flex justify-between items-center w-full h-full'>
-            <div className='flex flex-col gap-10 h-max mx-12 2xl:mx-28'>
-              <h1 className='text-4xl sm:text-5xl md:text-6xl mt-6 font-bold text-zinc-50 border-zinc-50 drop-shadow-sm'>
-                Rádio Mix Digital
-              </h1>
-
-              <p className='text-md sm:text-lg md:text-xl text-zinc-100 lg:max-w-[750px] drop-shadow-md'>
-                Seja bem-vindo ao nosso portal oficial, aqui você fica por
-                dentro de toda a nossa programação.
-              </p>
-            </div>
-
-            <div className='logo-img'>
-              <img
-                src='/radio-mix-digital-logo.jpeg'
-                height={225}
-                width={225}
-                alt='Radio Mix Digital'
-                className='rounded-3xl drop-shadow-lg mr-24 hidden lg:block'
-              />
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className='flex flex-col justify-center items-center w-full h-auto'>
         <section className='w-full h-[220px] flex justify-center items-end'></section>
