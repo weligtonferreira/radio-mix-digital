608d713a36c07f9d281184eda02c050b930eccb3
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-08-06 14:53:29 -0300

    feat: change resposity of the arrow up button component

--- src/components/arrow-up-button/index.tsx
+++ src/components/arrow-up-button/index.tsx
@@ -2,7 +2,7 @@ import { FaArrowUp } from 'react-icons/fa';
 
 export function ArrowUpButton() {
   return (
-    <div className='fixed right-10 bottom-10'>
+    <div className='fixed right-5 md:right-10 bottom-10'>
       <div
         onClick={() => window.scrollTo(0, 0)}
         className='flex justify-center items-center m-auto z-10 bg-gray-600 text-white w-12 h-12 cursor-pointer drop-shadow-md rounded-full hover:scale-125 transition duration-200'
