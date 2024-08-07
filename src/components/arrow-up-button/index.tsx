135ee8ab0596df1b991257ea47c8f9e4d53133fc
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-08-07 13:37:31 -0300

    refactor: change size class of arrow up button component

--- src/components/arrow-up-button/index.tsx
+++ src/components/arrow-up-button/index.tsx
@@ -18,7 +18,7 @@ export function ArrowUpButton() {
     <div className='fixed right-5 md:right-10 bottom-10'>
       <div
         onClick={() => window.scrollTo(0, 0)}
-        className={`flex justify-center items-center m-auto z-10 bg-gray-600 text-white w-12 h-12 cursor-pointer drop-shadow-md rounded-full ${scale} hover:scale-125 transition duration-200`}
+        className={`flex justify-center items-center m-auto z-10 bg-gray-600 text-white size-12 cursor-pointer drop-shadow-md rounded-full ${scale} hover:scale-125 transition duration-200`}
       >
         <FaArrowUp size={20} />
       </div>
