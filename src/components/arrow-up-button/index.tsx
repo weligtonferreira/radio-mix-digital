46be8ac41b6d789d7a7dc93c136fa0f1eabb77c2
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2024-09-24 10:31:00 -0300

    refactor(ArrowUpButton): add animation when component disappears

--- src/components/arrow-up-button/index.tsx
+++ src/components/arrow-up-button/index.tsx
@@ -2,23 +2,42 @@ import { useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
 
 export function ArrowUpButton() {
-  const [scale, setScale] = useState('scale-0');
+  const [isVisible, setIsVisible] = useState(false);
+  const [scaleClass, setScaleClass] = useState('scale-0');
+  const [opacityClass, setOpacityClass] = useState('opacity-0');
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setScale('scale-100');
-    }, 100);
+    if (isVisible) {
+      setScaleClass('scale-100');
+      setOpacityClass('opacity-100');
+    } else {
+      setScaleClass('scale-0');
+      setOpacityClass('opacity-0');
+    }
+  }, [isVisible]);
 
-    return () => {
-      clearInterval(timeout);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scroolPosition = window.scrollY;
+      const targetPosition = 600;
+
+      if (scroolPosition >= targetPosition) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
     };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <div className='fixed right-5 md:right-10 bottom-10'>
       <div
         onClick={() => window.scrollTo(0, 0)}
-        className={`flex justify-center items-center m-auto z-10 bg-gray-600 text-white size-12 cursor-pointer drop-shadow-md rounded-full ${scale} hover:scale-125 transition duration-200`}
+        className={`flex justify-center items-center m-auto z-10 bg-gray-600 text-white size-12 cursor-pointer drop-shadow-md rounded-full ${scaleClass} ${opacityClass} hover:scale-125 transition duration-200`}
       >
         <FaArrowUp size={20} />
       </div>
